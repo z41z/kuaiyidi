985cc0197d1f5c19cc1400aa886ed5728660c8b6
z41zP <isecfe@gmail.com>
2019-07-15 14:23:54 +0800

    ads

--- pages/search/index.js
+++ pages/search/index.js
@@ -39,9 +39,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    wx.setEnableDebug({
-      enableDebug: true
-    })
+    // wx.setEnableDebug({
+    //   enableDebug: true
+    // })
   },
 
   /**
